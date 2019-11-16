7b2c8d7ef5bb1cd988b30faafb52235ce8799fd0
Julien <julien.thomas018@live.fr>
2019-11-16 02:32:43 +0100

    Update main.js
    Tout cassé

--- js/main.js
+++ js/main.js
@@ -3363,7 +3363,7 @@ class V2C_CssEditor extends BDV2.reactComponent {
 
     componentDidMount() {
         // this.updateLineCount();
-        this.editor = ace.edit("bd-customcss-editor-detached");
+        this.editor = ace.edit("bd-customcss-editor");
         this.editor.setTheme("ace/theme/monokai");
         this.editor.session.setMode("ace/mode/css");
         this.editor.setShowPrintMargin(false);
